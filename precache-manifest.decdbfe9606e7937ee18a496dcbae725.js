7c24ac8a1e3731d37381a9db5172594686146284
Volodsher <volodsher@gmail.com>
2020-03-16 23:13:26 -0400

    Updates

---
+++ precache-manifest.decdbfe9606e7937ee18a496dcbae725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767ff48288f18e7ce59033ba400c3366",
+    "url": "./index.html"
+  },
+  {
+    "revision": "541a0715d460795dce09",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d25d7525b328ab26b312",
+    "url": "./static/js/2.fa69a746.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.fa69a746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541a0715d460795dce09",
+    "url": "./static/js/main.84b89135.chunk.js"
+  },
+  {
+    "revision": "9a4b401c30c61e4f9f4a",
+    "url": "./static/js/runtime-main.648fbf47.js"
+  }
+]);
